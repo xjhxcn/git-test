0d95c4c53fb8252e1ede7c6eb44231253260fd0c
Xjh <Xjh@atguigu.com>
2022-11-16 21:08:53 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/myGitHello.java
+++ src/main/java/com/atguigu/git/myGitHello.java
@@ -5,5 +5,7 @@ public class myGitHello {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
+
     }
 }
